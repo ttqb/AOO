98f734be73a8b02a051a5d48027891f452a4dd29
ttqb <146643561+ttqb@users.noreply.github.com>
2023-11-12 02:24:17 +0500

    Update 2.asm

--- ААС/ПЗ_9/2.asm
+++ ААС/ПЗ_9/2.asm
@@ -1,36 +1,38 @@
-%include 'io.inc'
-section .data
-section .bss
-str1 resb 100
-str2 resb 100
-section .text
-global CMAIN
+%include 'io.inc' ; Включение файла 'io.inc'
+
+section .data ; Секция данных
+section .bss ; Секция неинициализированных данных
+str1 resb 100 ; Резервирование 100 байтов под строку str1
+str2 resb 100 ; Резервирование 100 байтов под строку str2
+
+section .text ; Секция текста
+global CMAIN ; Глобальная функция CMAIN
+
 CMAIN:
-    mov ebp, esp            ;for correct debugging
-    xor eax, eax            ;обнуляем регистр
-    xor ebx, ebx            ;обнуляем регистр
-    xor ecx, ecx            ;обнуляем регистр
-    xor edx, edx            ;обнуляем регистр
-    GET_STRING str1,100     ;выводим str1 длинной в 100 символов
-    GET_STRING str2,100     ;выводим str2 длинной в 100 символов
+    mov ebp, esp ; для корректной отладки
+    xor eax, eax ; Обнуление регистра eax
+    xor ebx, ebx ; Обнуление регистра ebx
+    xor ecx, ecx ; Обнуление регистра ecx
+    xor edx, edx ; Обнуление регистра edx
+    GET_STRING str1,100 ; Получение строки str1 длиной 100 символов
+    GET_STRING str2,100 ; Получение строки str2 длиной 100 символов
 findend:
-    mov al, [str1+edx]      ;передвигаем в регистр al значение из строки str1
-    cmp al, 10              ;сравниваем регистр al с 10 (символ новой строки)
-    je concat               ;если равны конкатинация
-    inc edx                 ;
-    jmp findend
+    mov al, [str1+edx] ; Загрузка символа из строки str1 в al
+    cmp al, 10 ; Сравнение символа с ASCII-кодом новой строки (10)
+    je concat ; Если символ - новая строка, переход к метке 'concat'
+    inc edx ; Инкрементация edx
+    jmp findend ; Переход к метке 'findend'
 concat:
-    mov bl, [str2+ecx]
-    cmp bl,10
-    je print
-    mov [str1+edx],bl
-    inc ecx
-    inc edx
-    jmp concat
+    mov bl, [str2+ecx] ; Загрузка символа из строки str2 в bl
+    cmp bl,10 ; Сравнение символа с ASCII-кодом новой строки (10)
+    je print ; Если символ - новая строка, переход к метке 'print'
+    mov [str1+edx],bl ; Запись символа из bl в строку str1
+    inc ecx ; Инкрементация ecx
+    inc edx ; Инкрементация edx
+    jmp concat ; Переход к метке 'concat'
 print:
-    PRINT_STRING[str1]
-    xor eax,eax
-    xor ebx,ebx
-    xor ecx,ecx
-    ret
-    
+    PRINT_STRING[str1] ; Печать строки str1
+    xor eax,eax ; Обнуление регистра eax
+    xor ebx,ebx ; Обнуление регистра ebx
+    xor ecx,ecx ; Обнуление регистра ecx
+    ret ; Возврат из функции
