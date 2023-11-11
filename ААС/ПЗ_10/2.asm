f23d735b7d0b116fa343986e38a5d1beca751c82
ttqb <146643561+ttqb@users.noreply.github.com>
2023-11-12 02:31:08 +0500

    Update 2.asm

--- ААС/ПЗ_10/2.asm
+++ ААС/ПЗ_10/2.asm
@@ -1,62 +1,44 @@
-%include 'io.inc'
-section .data
-section .bss
-    array resd 4000
-    rows resd 1
-    cols resd 1
-    elemc resd 1
-section .text
-global CMAIN
+%include 'io.inc' ; Включение файла 'io.inc'
+
+section .data ; Секция данных
+section .bss ; Секция неинициализированных данных
+    array resd 4000 ; Резервирование 4000 двойных слов под массив
+    rows resd 1 ; Резервирование двойного слова под количество строк
+    cols resd 1 ; Резервирование двойного слова под количество столбцов
+    elemc resd 1 ; Резервирование двойного слова под количество элементов
+
+section .text ; Секция текста
+global CMAIN ; Глобальная функция CMAIN
+
 CMAIN:
-    mov ebp, esp; for correct debugging
-    xor eax,eax
-    xor ebx, ebx
-    xor ecx, ecx
+    mov ebp, esp ; для корректной отладки
+    xor eax,eax ; Обнуление регистра eax
+    xor ebx, ebx ; Обнуление регистра ebx
+    xor ecx, ecx ; Обнуление регистра ecx
     
-    GET_DEC 4, rows
-    GET_DEC 4, cols
+    GET_DEC 4, rows ; Получение количества строк
+    GET_DEC 4, cols ; Получение количества столбцов
     
-    mov eax, [rows]
-    mul dword [cols]
-    mov [elemc], eax
+    mov eax, [rows] ; Загрузка количества строк в eax
+    mul dword [cols] ; Умножение на количество столбцов
+    mov [elemc], eax ; Запись количества элементов
     
-    xor eax,eax
-    xor ebx, ebx
-    xor ecx, ecx
+    xor eax,eax ; Обнуление регистра eax
+    xor ebx, ebx ; Обнуление регистра ebx
+    xor ecx, ecx ; Обнуление регистра ecx
     
 createarr:
-    GET_DEC 4, eax
-    cmp ebx,[elemc]
-    jg created
-    mov [array + ebx + (3 * ebx)], eax
-    inc ebx
-    cmp ebx,[elemc]
-    jne createarr
+    GET_DEC 4, eax ; Получение значения элемента
+    cmp ebx,[elemc] ; Сравнение ebx с количеством элементов
+    jg created ; Если ebx больше количества элементов, переход к метке 'created'
+    mov [array + ebx + (3 * ebx)], eax ; Запись значения элемента в массив
+    inc ebx ; Инкрементация ebx
+    cmp ebx,[elemc] ; Сравнение ebx с количеством элементов
+    jne createarr ; Если не равны, переход к метке 'createarr'
     
 created:
-    xor eax, eax
-    xor ebx, ebx
-    xor ecx, ecx
-    xor edx, edx
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    ret
+    xor eax, eax ; Обнуление регистра eax
+    xor ebx, ebx ; Обнуление регистра ebx
+    xor ecx, ecx ; Обнуление регистра ecx
+    xor edx, edx ; Обнуление регистра edx
+    ret ; Возврат из функции
