7f39685ab46376098ec601395aa2dcc48b534d07
ttqb <146643561+ttqb@users.noreply.github.com>
2023-11-12 02:33:24 +0500

    Update 3.asm

--- ААС/ПЗ_10/3.asm
+++ ААС/ПЗ_10/3.asm
@@ -1,30 +1,30 @@
-%include 'io.inc'
+%include 'io.inc' ; Включение файла 'io.inc'
 
-section .data
-    array db 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
-    len equ $-array
+section .data ; Секция данных
+    array db 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ; Массив данных
+    len equ $-array ; Длина массива
 
-section .text
-    global CMAIN
+section .text ; Секция текста
+    global CMAIN ; Глобальная функция CMAIN
 CMAIN:
-    mov ecx, len
-    mov esi, array
-    xor edi, edi
-    mov eax, [esi]
+    mov ecx, len ; Загрузка длины массива в ecx
+    mov esi, array ; Загрузка адреса массива в esi
+    xor edi, edi ; Обнуление регистра edi
+    mov eax, [esi] ; Загрузка первого элемента массива в eax
 next:
-    cmp eax, [esi+edi]
-    jg swap
-    jmp cont
+    cmp eax, [esi+edi] ; Сравнение текущего элемента массива с eax
+    jg swap ; Если текущий элемент больше, переход к метке 'swap'
+    jmp cont ; Если текущий элемент не больше, переход к метке 'cont'
 swap:
-    mov eax, [esi+edi]
-    mov ebx, edi
+    mov eax, [esi+edi] ; Загрузка текущего элемента в eax
+    mov ebx, edi ; Загрузка текущего индекса в ebx
 cont:
-    add edi, 4
-    loop next
+    add edi, 4 ; Увеличение индекса на 4
+    loop next ; Переход к метке 'next', пока ecx не равен 0
 
-    mov edx, [esi]
-    mov [esi], eax
-    mov [esi+ebx], edx
+    mov edx, [esi] ; Загрузка первого элемента массива в edx
+    mov [esi], eax ; Запись минимального элемента в первую ячейку массива
+    mov [esi+ebx], edx ; Запись первого элемента на место минимального
 
-    mov eax, 1
-    int 0x80
+    mov eax, 1 ; Загрузка 1 в eax
+    int 0x80 ; Вызов прерывания 0x80 для завершения программы
