cadafe3210d4a44c3fe6c5c10d6dda7cd9b674e7
ttqb <146643561+ttqb@users.noreply.github.com>
2023-11-12 02:40:14 +0500

    Update 3.asm

--- ААС/ПЗ_11/3.asm
+++ ААС/ПЗ_11/3.asm
@@ -1,28 +1,31 @@
-%include 'io.inc'
-section .data
-section .bss
-    string resb 1000
-section .text
-global CMAIN
+%include 'io.inc' ; Включение файла 'io.inc'
+
+section .data ; Секция данных
+section .bss ; Секция неинициализированных данных
+    string resb 1000 ; Резервирование 1000 байтов под строку
+
+section .text ; Секция текста
+global CMAIN ; Глобальная функция CMAIN
+
 CMAIN:
-    mov ebp, esp; for correct debugging
-    xor eax,eax
-    xor ebx,ebx
-    xor ecx,ecx
-    xor edx,edx
-    GET_STRING string,100
+    mov ebp, esp ; для корректной отладки
+    xor eax,eax ; Обнуление регистра eax
+    xor ebx,ebx ; Обнуление регистра ebx
+    xor ecx,ecx ; Обнуление регистра ecx
+    xor edx,edx ; Обнуление регистра edx
+    GET_STRING string,100 ; Получение строки от пользователя длиной 100 символов
 loopa:
-    mov ebx, string
-    cmp byte[ebx+ecx],0
-    je cont
-    inc ecx
-    jmp loopa
+    mov ebx, string ; Загрузка адреса строки в ebx
+    cmp byte[ebx+ecx],0 ; Сравнение текущего символа строки с нулем (конец строки)
+    je cont ; Если текущий символ - ноль, переход к метке 'cont'
+    inc ecx ; Инкрементация ecx
+    jmp loopa ; Переход к метке 'loopa'
 cont:
-    PRINT_CHAR [ebx+ecx]
-    dec ecx
-    cmp byte[ebx+ecx],0
-    je end
-    jmp cont
+    PRINT_CHAR [ebx+ecx] ; Печать текущего символа строки
+    dec ecx ; Декрементация ecx
+    cmp byte[ebx+ecx],0 ; Сравнение текущего символа строки с нулем (конец строки)
+    je end ; Если текущий символ - ноль, переход к метке 'end'
+    jmp cont ; Переход к метке 'cont'
 end:
-    xor eax,eax
-    ret
+    xor eax,eax ; Обнуление регистра eax
+    ret ; Возврат из функции
